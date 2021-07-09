fedaf5953357f31bf09a76b07a59b19ef3c1099d
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2021-07-08 19:36:37 -0700

    Add special command, "join last sentence".

--- BarkerCustomSpeechTest/MainWindow.xaml.cs
+++ BarkerCustomSpeechTest/MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace BarkerCustomSpeechTest
         private SpeechRecognizer recognizer;
         private DispatcherTimer timeoutTimer;
         private int timeoutTimerTimeout = 30;
+        private DateTime timeOfLastRecognition;
 
         public MainWindow()
         {
@@ -61,6 +62,7 @@ namespace BarkerCustomSpeechTest
                 // a long time.
 
                 // The timeout timer is started on the UI thread.
+                timeOfLastRecognition = DateTime.Now;
                 timeoutTimer.Start();
 
                 SpeakButton.Content = "_Stop speaking";
@@ -102,18 +104,14 @@ namespace BarkerCustomSpeechTest
         // GetSpeechResult() is running on a background thread.
         private async Task GetSpeechResult()
         {
-            var result = await GetSpeechInputDefault(
+            await GetSpeechInputDefault(
                 subscriptionEndpointId,
                 subscriptionKey,
                 region,
                 showConfidenceWithResults);
-
-            // Show any speech recognition results in the UI on the UI thread.
-            Application.Current.Dispatcher.Invoke(
-                new Action(() => { RecoSpeechLabel.Text = result; }));
         }
 
-        public async Task<string> GetSpeechInputDefault(
+        public async Task GetSpeechInputDefault(
             string subscriptionEndpointId,
             string subscriptionKey,
             string region,
@@ -151,9 +149,18 @@ namespace BarkerCustomSpeechTest
                 {
                     if (e.Result.Reason == ResultReason.RecognizedSpeech)
                     {
-                        Debug.WriteLine("Recognized: " + e.Result.Text);
+                        timeOfLastRecognition = DateTime.Now;
 
-                        speechInput += (speechInput.Length == 0 ? "" : " ") + e.Result.Text;
+                        if (!IsSpecialCommand(e.Result.Text, ref speechInput))
+                        {
+                            Debug.WriteLine("Recognized: " + e.Result.Text);
+
+                            speechInput += (speechInput.Length == 0 ? "" : " ") + e.Result.Text;
+                        }
+
+                        // Continually build up the entire recognized sentence.
+                        Application.Current.Dispatcher.Invoke(
+                            new Action(() => { RecoSpeechLabel.Text = speechInput; }));
                     }
                     // ResultReason.NoMatch only seems to get raised when speech occurs
                     // after a pause.
@@ -186,12 +193,61 @@ namespace BarkerCustomSpeechTest
                 Debug.WriteLine("Attempt to recognize speech failed. " + ex.Message);
             }
 
-            return speechInput;
+            Debug.WriteLine("Final recognized phrase is: " + speechInput);
+        }
+
+        private bool IsSpecialCommand(string utterance, ref string speechInput)
+        {
+            bool isSpecialCommand = false;
+
+            if (utterance.Trim().ToLower().StartsWith("join last sentence"))
+            {
+                StringBuilder sb = new StringBuilder(speechInput);
+                if (sb.Length > 1)
+                {
+                    // Ignore the last character in the string. We never want
+                    // to change that character, regardless of what it is.
+                    for (int i = sb.Length - 2; i > 0; --i)
+                    {
+                        if (sb[i] == '.')
+                        {
+                            sb[i] = ',';
+
+                            // If the next character is upper case, change it to lower case.
+                            for (int j = i + 1; j < sb.Length - 2; ++j)
+                            {
+                                if (sb[j] != ' ')
+                                {
+                                    if ((sb[j] >= 65 ) && (sb[j] <= 90))
+                                    {
+                                        sb[j] = (char)(sb[j] + 32); 
+                                    }
+
+                                    break;
+                                }
+                            }
+
+                            break;
+                        }
+                    }
+
+                    speechInput = sb.ToString();
+                }
+
+                isSpecialCommand = true;
+            }
+
+            return isSpecialCommand;
         }
 
         private void TimeoutTimer_Tick(object sender, EventArgs e)
         {
-            StopRecognition();
+            TimeSpan span = DateTime.Now - timeOfLastRecognition;
+
+            if (span.TotalSeconds > 30)
+            {
+                StopRecognition();
+            }
         }
 
         private void SettingsButton_Clicked(object sender, EventArgs e)
